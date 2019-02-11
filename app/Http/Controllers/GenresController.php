b329d0d549b7795a187b3fbc1c4765cc36facbaf
zakkarys <43589100+zakkarys@users.noreply.github.com>
2019-02-10 17:15:40 -0800

    Add files via upload

--- app/Http/Controllers/GenresController.php
+++ app/Http/Controllers/GenresController.php
@@ -1,19 +1,41 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use DB;
-
-
+use Validator;
 class GenresController extends Controller
 {
-    public function index(){
+    public function index(Request $request)
+    {
+        $query = DB::table('genres')->select('Name', 'GenreId');
+        $genres = $query->get();
+        return view('genres', [
+           'genres' => $genres
+        ]);
+    }
+    public function edit($genreid) {
+       $genre =  DB::table('genres')->where('GenreId', '=', $genreid)->first();
+       return view('editGenre', [
+           'genre' => $genre
+       ]);
+    }
+    public function store($genreid, Request $request) {
+       
+        $input = $request->all();
+        $validation = Validator::make($input, [
+            'genre' => 'required|min:3|unique:genres,Name'
+        ]);
 
-    	$genres = DB::table('genres')->get();
-
-    	return view('index', [
-    		'genres' => $genres
-    	]);
+        if($validation->fails()) {
+            return redirect('/genres/'.$genreid.'/edit')
+                ->withInput()
+                ->withErrors($validation);
+        }
+   
+        DB::table('genres')
+            ->where('GenreId', '=', $genreid)
+            ->update(['Name' => $request->genre]);
+    
+        return redirect('./genres');
     }
 }
--- app/Http/Controllers/TracksController.php
+++ app/Http/Controllers/TracksController.php
@@ -1,23 +1,77 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use DB;
-
-
+use Validator;
 class TracksController extends Controller
 {
-    public function index(){
-    	$sGenre = urldecode($_GET["genre"]);
-    	$query1 = DB::table('tracks')->select('tracks.Name AS TrackName', 'albums.Title AS AlbumTitle', 'artists.Name AS ArtistsName', 'tracks.UnitPrice AS UnitPrice');
-    	$query2 = $query1->join('genres', 'tracks.GenreId', '=', 'genres.GenreId');
-    	$query3 = $query2->join('albums', 'tracks.AlbumId', '=', 'albums.AlbumId');
-    	$query4 = $query3->join('artists', 'albums.ArtistId', '=', 'artists.ArtistId');
-    	$query5 = $query4->where('genres.Name', '=', $sGenre);
-    	$tracks = $query5->get();
-    	return view('tracks', [
-    		'tracks' => $tracks
-    	]);
+    public function index(Request $request)
+    {
+        $query = DB::table('tracks');
+       
+        if($request->query('genre')) {
+            $query->join('genres', function($join) use ($request) {
+                        $join
+                            ->on('genres.GenreId', '=', 'tracks.GenreId')
+                            ->on('genres.Name', '=', $request->query('genre'));
+                    });
+        }
+        $query->join('albums', 'albums.AlbumId', '=', 'tracks.AlbumId');
+        $query->join('artists', 'artists.ArtistId', '=', 'albums.ArtistId');
+        $query->select('tracks.Name as trackName', 'tracks.UnitPrice as price', 'albums.Title as title', 'artists.Name as artistName');
+        $tracks = $query->get();
+        return view('tracks', [
+            'tracks' => $tracks,
+            'genre' => $request->query('genre')
+        ]);
+    }
+    public function addTrack(Request $request) {
+    
+        $query_album = DB::table('albums');
+        $albums = $query_album->get();
+        $query_media = DB::table('media_types');
+        $media = $query_media->get();
+        $query_genre = DB::table('genres');
+        $genres = $query_genre->get();
+        return view('addTracks.addTrack', [
+            'albums' => $albums,
+            'media' => $media,
+            'genres' => $genres
+        ]);
+    }
+    public function store(Request $request) {
+     
+        $input = $request->all();
+        $validation = Validator::make($input, [
+            'name' => 'required',
+            'albums' => 'required',
+            'media' => 'required',
+            'genres' => 'required',
+            'composer' => 'required',
+            'mili' => 'required|integer',
+            'bytes' => 'required|integer',
+            'unit' => 'required|integer'
+        ]);
+      
+        if($validation->fails()) {
+            return redirect('/tracks/new')
+                ->withInput()
+                ->withErrors($validation);
+        }
+
+        DB::table('tracks')->insert(
+            [
+                'Name' => $request->name,
+                'AlbumId' => $request->albums,
+                'MediaTypeId' => $request->media,
+                'GenreId' => $request->genres,
+                'Composer' => $request->composer,
+                'Milliseconds' => $request->mili,
+                'Bytes' => $request->bytes,
+                'UnitPrice' => $request->unit
+            ]
+        );
+   
+        return redirect('./tracks');
     }
 }
