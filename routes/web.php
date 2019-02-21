cc31758adec695fab9270083e7ef61b12f3fa025
zakkarys <43589100+zakkarys@users.noreply.github.com>
2019-02-21 14:00:25 -0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/genres', 'GenresController@index');
-Route::get('/genres/{id}/edit', 'GenresController@edit');
-Route::post('/genres/{id}/store', 'GenresController@store');
-Route::get('/tracks', 'TracksController@index');
-Route::get('/tracks/new', 'TracksController@addTrack');
-Route::post('/tracks/store', 'TracksController@store');
+Route::get('/login', 'LoginController@index');
+Route::post('/login', 'LoginController@login');
+Route::get('/logout', 'LoginController@logout');
+Route::get('/maintenance', 'ConfigurationController@index');
+Route::middleware(['authenticated'])->group(function () {
+    Route::get('/profile', 'AdminController@index');
+    Route::get('/settings', 'SettingController@index');
+    Route::post('/settings/update', 'SettingController@update');
+});
+Route::middleware(['maintenance'])->group(function () {
+    Route::get('/', 'GenresController@index');
+    Route::get('/genres', 'GenresController@index');
+    Route::get('/genres/{id}/edit', 'GenresController@edit');
+    Route::post('/genres/{id}/store', 'GenresController@store');
+    Route::get('/tracks', 'TracksController@index');
+    Route::get('/tracks/new', 'TracksController@addTrack');
+    Route::post('/tracks/store', 'TracksController@store');
+    Route::get('/signup', 'SignUpController@index');
+    Route::post('/signup', 'SignUpController@signup');
+});
