2249c99481e844ded8cb44a9ee679a7a5d743480
zakkarys <43589100+zakkarys@users.noreply.github.com>
2019-02-21 13:45:53 -0800

    Add files via upload

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,9 +1,6 @@
 <?php
-
 namespace App\Http;
-
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-
 class Kernel extends HttpKernel
 {
     /**
@@ -20,7 +17,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
     ];
-
     /**
      * The application's route middleware groups.
      *
@@ -36,13 +32,11 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
         'api' => [
             'throttle:60,1',
             'bindings',
         ],
     ];
-
     /**
      * The application's route middleware.
      *
@@ -60,8 +54,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'authenticated' => \App\Http\Middleware\Authenticated::class,
+        'maintenance' => \App\Http\Middleware\MaintenanceMode::class,
     ];
-
     /**
      * The priority-sorted list of middleware.
      *
